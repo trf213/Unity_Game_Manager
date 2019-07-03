686081e7043afcde8610bba4821f4f0199b032f4
trf213 <tabari233@gmail.com>
2019-07-03 17:57:36 -0400

    Character Select Prototype

---
+++ GameManager Api/characterSelect/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+   private int index;
+   private bool isUnlocked;
+   private int price;
+
+
+
+   
+  public bool getisUnlocked()
+  {
+      return isUnlocked;
+  }
+    public void setisUnlocked(bool val)
+  {
+   isUnlocked = val;
+  }
+   public int getIndex()
+   {
+       return index;
+   }
+
+   public int getprice()
+   {
+     return price;
+   }
+}
---
+++ GameManager Api/characterSelect/characterSelect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterSelect : MonoBehaviour
+{
+    public int index;
+   public  Character[] mycharacteList;
+   
+   private int indexCheck;
+
+  
+   public int Range()
+   {
+       if ( mycharacteList.Length <= 0 )
+          Debug.Log("May want to populate me");
+       return mycharacteList.Length;
+   }
+
+   public int select()
+    {
+        if(index == 0)
+        return 0;
+
+         return mycharacteList[indexCheck].getIndex();
+    }
+
+   public void nextRight()
+   {
+        if(index == 0)
+        return ;
+     if(indexCheck == mycharacteList.Length)
+     {
+         indexCheck = 0;
+     }
+     else {indexCheck++;}
+
+   }
+
+   public void nextLeft()
+   {
+           if(index == 0)
+        return ;
+     if(indexCheck == mycharacteList.Length)
+     {
+         indexCheck = 0;
+     }
+     else {indexCheck++;}
+   }
+
+   public void setSelect(int val)
+   {
+       indexCheck = val;
+
+   }
+}
